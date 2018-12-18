577c4124ddc43270fed46a72e651ec4c85ecca0a
Jim Hart <HART0554@UMN.EDU>
2018-12-18 12:36:17 -0600

    adjust component options and styling

--- modules/folwell_wysiwyg/js/plugins/fwcallout/dialogs/fwcallout.js
+++ modules/folwell_wysiwyg/js/plugins/fwcallout/dialogs/fwcallout.js
@@ -18,11 +18,47 @@
                   ['Top Border', 'callout-border-top'],
                   ['Left Border', 'callout-border-left']
               ],
+              'default': 'callout-background',
               setup: function(widget) {
                 this.setValue(widget.data.style);
               },
               commit: function(widget) {
                 widget.setData('style', this.getValue());
+              }
+            },
+            {
+              id: 'width',
+              type: 'select',
+              label: 'Select callout width',
+              items: [
+                ['Full', 'display-100'],
+                ['50%', 'display-50'],
+                ['33%', 'display-33'],
+                ['25%', 'display-25']
+              ],
+              'default': 'display-100',
+              setup: function(widget) {
+                this.setValue(widget.data.width);
+              },
+              commit: function(widget) {
+                widget.setData('width', this.getValue());
+              }
+            },
+            {
+              id: 'align',
+              type: 'select',
+              label: 'Select callout float',
+              items: [
+                ['None', 'align-none'],
+                ['Left', 'align-left'],
+                ['Right', 'align-right']
+              ],
+              'default': 'align-none',
+              setup: function(widget) {
+                this.setValue(widget.data.align);
+              },
+              commit: function(widget) {
+                widget.setData('align', this.getValue());
               }
             }
           ]
--- modules/folwell_wysiwyg/js/plugins/fwcallout/plugin.js
+++ modules/folwell_wysiwyg/js/plugins/fwcallout/plugin.js
@@ -12,8 +12,13 @@
       editor.widgets.add('fwcallout', {
         button: 'Add a callout box',
         dialog: 'fwcallout',
+        defaults: {
+          style: 'callout-background',
+          width: 'display-100',
+          align: 'align-none'
+        },
         template:
-          '<aside class="callout callout-background">' +
+          '<aside class="callout callout-background display-100 align-none">' +
             '<header class="callout-header"><h1>Header</h1></header>' +
             '<div class="callout-content"><p>Content</p></div>' +
           '</aside>',
@@ -27,7 +32,7 @@
             allowedContent: 'p br ul ol li strong em div'
           }
         },
-        allowedContent: 'aside(!callout,callout-background,callout-border-all,callout-border-top,callout-border-left); header(!callout-header); div(!callout-content)',
+        allowedContent: 'aside(!callout,callout-background,callout-border-all,callout-border-top,callout-border-left,display-100,display-50,display-33,display-25,align-none,align-right,align-left); header(!callout-header); div(!callout-content)',
         requiredContent: 'aside(!callout); div(!callout-content)',
         upcast: function(element) {
           return element.name == 'aside' && element.hasClass('callout');
@@ -42,14 +47,43 @@
           } else if (this.element.hasClass('callout-border-left')) {
             this.setData('style', 'callout-border-left');
           }
+          if (this.element.hasClass('display-100')) {
+            this.setData('style', 'display-100');
+          } else if (this.element.hasClass('display-50')) {
+            this.setData('style', 'display-50');
+          } else if (this.element.hasClass('display-33')) {
+            this.setData('style', 'display-33');
+          } else if (this.element.hasClass('display-25')) {
+            this.setData('style', 'display-25');
+          }
+          if (this.element.hasClass('align-none')) {
+            this.setData('style', 'align-none');
+          } else if (this.element.hasClass('align-right')) {
+            this.setData('style', 'align-right');
+          } else if (this.element.hasClass('align-left')) {
+            this.setData('style', 'align-left');
+          }
         },
         data: function() {
           this.element.removeClass('callout-background');
           this.element.removeClass('callout-border-all');
           this.element.removeClass('callout-border-top');
           this.element.removeClass('callout-border-left');
+          this.element.removeClass('display-100');
+          this.element.removeClass('display-50');
+          this.element.removeClass('display-33');
+          this.element.removeClass('display-25');
+          this.element.removeClass('align-none');
+          this.element.removeClass('align-right');
+          this.element.removeClass('align-left');
           if (this.data.style) {
             this.element.addClass(this.data.style);
+          }
+          if (this.data.width) {
+            this.element.addClass(this.data.width);
+          }
+          if (this.data.align) {
+            this.element.addClass(this.data.align);
           }
         }
       });
